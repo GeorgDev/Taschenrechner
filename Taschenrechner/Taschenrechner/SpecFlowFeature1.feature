78dee5791a4508db032386b73336fa92c966d2e0
Georg <georg.pretzner@gmail.com>
2020-11-02 18:56:37 +0100

    msg

--- Taschenrechner/Taschenrechner/SpecFlowFeature1.feature
+++ Taschenrechner/Taschenrechner/SpecFlowFeature1.feature
@@ -1,10 +1,28 @@
-﻿Feature: SpecFlowFeature1
+﻿Feature: Taschenrechner
 	In order to avoid silly mistakes
 	As a math idiot
-	I want to be told the sum of two numbers
+	I want to be use trigonometric functions
 
-@mytag
-Scenario: Add two numbers
+
+Scenario: use pi
+	Given the first number is 0
+	And the second number is 70
+	When the cosinus is applied on the first number
+	Then the result should be 1
+
+Scenario: use sin
+	Given the first number is 0
+	And the second number is 70
+	When the cosinus is applied on the first number
+	Then the result should be 0
+
+Scenario: use cos
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+
+Scenario: use tan
 	Given the first number is 50
 	And the second number is 70
 	When the two numbers are added
---
+++ Taschenrechner/Taschenrechner/TaschenrechnerSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Taschenrechner
+{
+    [Binding]
+    public class TaschenrechnerSteps
+    {
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the cosinus is applied on the first number")]
+        public void WhenTheCosinusIsAppliedOnTheFirstNumber()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
